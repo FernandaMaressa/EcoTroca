fa56322b02ed38ea9c6be29351bb09de2797ceec
FernandaMaressa <fernandamaressa04@gmail.com>
2025-07-11 15:23:48 -0300

    refactor: alterando as imgs que estão expostas na página do perfil do usuário e modificando as descrições das imagens

--- src/pages/perfil/Perfil.jsx
+++ src/pages/perfil/Perfil.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Perfil.css";
+import batman from "../../assets/batman.png";
+import capacete from "../../assets/capacete.png";
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
 import logo from "../../assets/logo.svg";
@@ -32,7 +34,7 @@ export default function Perfil() {
         <h3>Meus Itens</h3>
         <div className="meus-itens">
           <div className="item-card">
-            <img src="https://images.unsplash.com/photo-1560807707-8cc77767d783?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5&ixlib=rb-4.0.3&q=80&w=400" alt="Item"/>
+            <img src={batman} alt="Item Batman"/>
             <div className="item-info">
               <h4>Boneco Batman</h4>
               <p>Meu filho ganhou dois iguais.</p>
@@ -46,7 +48,7 @@ export default function Perfil() {
           </div>
 
           <div className="item-card">
-            <img src="https://images.unsplash.com/photo-1608393681318-f6e78b5f9293?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5&ixlib=rb-4.0.3&q=80&w=400" alt="Item"/>
+            <img src={capacete} alt="Item Capacete"/>
             <div className="item-info">
               <h4>Capacete</h4>
               <p>Usado por 1 ano e alguns meses.</p>
