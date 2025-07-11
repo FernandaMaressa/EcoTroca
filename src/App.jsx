13a0a8d6121aa2dff98b861b2641599c0cf46a4c
FernandaMaressa <fernandamaressa04@gmail.com>
2025-07-11 14:44:10 -0300

    fix: corrigindo bug de caminho depois da alteração da pagina Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Register />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
