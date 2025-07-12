781029003edebb4668868b6ebb59d04206839701
Maciel <macielsemr@gmail.com>
2025-07-12 13:53:49 -0300

    feat: iniciando pagina de listagem de itens

--- src/pages/itens/Itens.jsx
+++ src/pages/itens/Itens.jsx
@@ -1,12 +1,19 @@
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
+import { Search } from "lucide-react";
 
 export default function Itens() {
   return (
     <section>
       <NavBar />
-      <div>página</div>
-      <div>itens</div>
+      <div>
+        <h2>Explorar itens</h2>
+        <p>Encontre itens incríveis para trocar</p>
+      </div>
+      <div>
+        <input type="text" name="buscarItem" id="buscarItem" />
+      </div>
+      <section>lista de itens</section>
       <Footer />
     </section>
   );
