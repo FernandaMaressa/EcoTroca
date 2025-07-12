4e1a2733340e536f17262263748132d8aaa9f9bd
Maciel <macielsemr@gmail.com>
2025-07-12 13:45:04 -0300

    fix: ajuste de seções da página home

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,10 +1,71 @@
 import React from "react";
+import { MapPin } from "lucide-react";
 
-export default function CardItem() {
+export default function CardItem({ imagem, titulo, desc, local, badge }) {
   return (
-    <div style={{ border: "1px solid #ddd", padding: "1rem", borderRadius: "8px" }}>
-      <h3>Item Exemplo</h3>
-      <p>Descrição blabla blabla</p>
+    <div
+      style={{
+        border: "1px solid #ddd",
+        borderRadius: "8px",
+      }}
+    >
+      <div
+        style={{
+          width: 210,
+          height: 200,
+          borderTopRightRadius: 8,
+          borderTopLeftRadius: 8,
+          position: "relative",
+          overflow: "hidden",
+        }}
+      >
+        <img
+          src={imagem}
+          alt="Imagem do Produto"
+          style={{
+            objectFit: "cover",
+            width: "100%",
+            height: "100%",
+            display: "block",
+          }}
+        />
+      </div>
+      <div
+        style={{
+          padding: "1rem",
+        }}
+      >
+        <h3>{titulo}</h3>
+        <p style={{ paddingTop: "5px" }}>{desc}</p>
+        <p style={{ display: "flex", gap: 5, alignItems: "center" }}>
+          <MapPin width={20} /> {local}
+        </p>
+        <p
+          style={{
+            color: "#43b3a0",
+            backgroundColor: "#D1FAE5",
+            padding: "2px 3px",
+            borderRadius: 8,
+            textAlign: "center",
+            width: "100px",
+          }}
+        >
+          {badge}
+        </p>
+        <a
+          href=""
+          style={{
+            textDecoration: "none",
+            backgroundColor: "#43b3a0",
+            color: "#fff",
+            padding: "0.5rem 1.7rem",
+            borderRadius: 12,
+            cursor: "pointer",
+          }}
+        >
+          Ver detalhes
+        </a>
+      </div>
     </div>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,17 +4,26 @@ import "./Home.css";
 import NavBar from "../../components/NavBar";
 import CardItem from "../../components/CardItem";
 import Footer from "../../components/Footer";
+import { Leaf, RefreshCcw, UsersRound } from "lucide-react";
+import img1 from "../../assets/batman.png";
+import img2 from "../../assets/capacete.png";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
-
       <section className="home-banner">
-        <img src={logo} alt="EcoTroca" />
-        <h1>Bem-vindo à <span>EcoTroca</span></h1>
-        <p>Conectamos pessoas para trocas sustentáveis.<br/>
-        Dê nova vida aos seus itens usados e encontre tesouros únicos.</p>
+        <div className="top-banner">
+          <img src={logo} alt="EcoTroca" width={70} />
+          <h1>
+            Bem-vindo à <span>EcoTroca</span>
+          </h1>
+          <p>
+            Conectamos pessoas para trocas sustentáveis.
+            <br />
+            Dê nova vida aos seus itens usados e encontre tesouros únicos.
+          </p>
+        </div>
 
         <div className="search-box">
           <input type="text" placeholder="O que você está procurando?" />
@@ -23,14 +32,17 @@ export default function Home() {
 
         <div className="stats-container">
           <div className="stat-card">
+            <UsersRound color="#43b3a0" />
             <h3>1.2K+</h3>
             <p>Usuários ativos</p>
           </div>
           <div className="stat-card">
+            <RefreshCcw color="#43b3a0" />
             <h3>3.5K+</h3>
             <p>Trocas realizadas</p>
           </div>
           <div className="stat-card">
+            <Leaf color="#43b3a0" />
             <h3>850+</h3>
             <p>Itens disponíveis</p>
           </div>
@@ -40,30 +52,53 @@ export default function Home() {
       <section className="itens-recentes">
         <h2>Itens Recentes</h2>
         <div className="cards">
-          <CardItem />
-          <CardItem />
-          <CardItem />
+          <CardItem
+            imagem={img1}
+            titulo="Boneco"
+            desc="Item usado"
+            local="Fortaleza-CE"
+            badge="Brinquedo"
+          />
+          <CardItem
+            imagem={img2}
+            titulo="Capacete"
+            desc="Seminovo disponível para troca"
+            local="Fortaleza-CE"
+            badge="Acesórios"
+          />
+          <CardItem
+            imagem={img1}
+            titulo="Boneco"
+            desc="Item usado"
+            local="Fortaleza-CE"
+            badge="Brinquedo"
+          />
         </div>
       </section>
 
       <section className="como-funciona">
         <h2>Como funciona?</h2>
         <div className="steps">
-          <div>
-            <h3>1</h3>
-            <p>Cadastre seus itens</p>
+          <div className="info-cont">
+            <h3 className="cont-num">1</h3>
+            <h4>Cadastre seus itens</h4>
+            <p>Publique fotos e descrições dos itens que deseja trocar</p>
           </div>
-          <div>
-            <h3>2</h3>
-            <p>Encontre interessados</p>
+          <div className="info-cont">
+            <h3 className="cont-num">2</h3>
+            <h4>Encontre interessados</h4>
+            <p>
+              Navegue por itens de outros usuários e encontre trocas
+              interessantes
+            </p>
           </div>
-          <div>
-            <h3>3</h3>
-            <p>Realize a troca</p>
+          <div className="info-cont">
+            <h3 className="cont-num">3</h3>
+            <h4>Realize a troca</h4>
+            <p>Entre em contato e combine os detalhes da troca</p>
           </div>
         </div>
       </section>
-
       <Footer />
     </div>
   );
